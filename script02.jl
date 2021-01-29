564797943ae9143d62e065df4c3d326ab4c60f6b
Jacob Wood <jacob.wood27@gmail.com>
2021-01-29 13:38:52 -0800

    After 2nd run

--- script02.jl
+++ script02.jl
@@ -1,5 +1,5 @@
 using Random
-using CSV
+using CSV, DataFrames
 
 # Test Case
 # BOOKS = ["book" * string(i) for i in 1:4]
@@ -8,7 +8,7 @@ using CSV
 csv_file = "books.csv"
 csv_out = "sorted_books.csv"
 
-books_table = CSV.read(csv_file)
+books_table = CSV.read(csv_file, DataFrame)
 BOOKS = books_table.Title
 
 function getc1()
@@ -33,26 +33,26 @@ for i in order[2:end]
         comp = rand(lb:ub)
         comp_book = BOOKS[RANKS[comp]]
 
-        println("(a): ", comp_book, "     or     (b): ", this_book)
+        println("(a): ", this_book, "  or  (b): ", comp_book)
         ui = getc1()
-        println(ui)
+        # println(ui)
 
         # Need to catch the case where we have no preceder
-        if comp==lb && ui=='b'
+        if comp==lb && ui=='a'
             insert!(RANKS,lb,i)
             break
         end
 
         # Catch the tail case
-        if comp==ub && ui=='a'
+        if comp==ub && ui=='b'
             insert!(RANKS,ub+1,i)
             break
         end
 
 
-        if ui=='b'
+        if ui=='a'
             ub = comp-1
-        elseif ui=='a'
+        elseif ui=='b'
             lb = comp+1
         elseif ui=='q'
             break
@@ -77,5 +77,5 @@ println(RANKS)
 
 # Write ordered csv
 books_table = books_table[RANKS,:]
-books_table["Rank"] = 1:length(RANKS)
+books_table[!, "Rank"] = 1:length(RANKS)
 CSV.write(csv_out, books_table)
